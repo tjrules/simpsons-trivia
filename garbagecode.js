9d3264a3f1bcaa41994220dc673efb483f01bfaa
TJ Stubbs <stubbsrule@gmail.com>
2017-12-09 15:46:35 -0500

    trying to loop through question choices object to create answer buttons

---
+++ garbagecode.js
@@ -0,0 +1,53 @@
+//creates start game
+//  function startGame() {let $startGame = $('div').addClass('wrapper').append(`<div class='start hide-start'>&nbsp<form id='wrapper'>
+// 		<legend class='hide-start'>Hello Simpson's Lover: What is your Name?</legend>
+// 		<input class='hide-start' type="name" name="">
+// 		<input class ='button hide-start' type="submit" name="">
+// 	</form>&nbsp
+// 	</div>`);}
+// startGame();
+
+//  function Create
+//   const $form = ('form')
+//   function grabForm(event) {
+//   event.preventDefault();
+//   console.log('$form.val()')
+
+// }
+
+//   function createQuestion() {let $createQuestion = $('div').addClass('wrapper').append(` <div class="question">What is Homer's catchphrase?</div>
+//   <div class="question-wrapper">
+//       <div class="answer">
+//         <button class='button' type='submit'>D'oh</button>
+//       </div>
+//       <div class="answer">
+//         <button class='button' type='submit'>Eat My Shorts</button>
+//       </div>
+//       <div class="answer">
+//         <button class='button' type='submit'>Aye Caramba</button>
+//       </div><div class="answer">
+//         <button class='button' type='submit'>That's Illogical</button>
+//       </div>`
+//                                                                                       );}
+
+//    function empty(){
+//      $('#wrapper').empty();
+//   }
+
+//   createQuestion();
+
+
+// let $name = $('#name-input').val();
+// let $changeName = $('legend').append($name);
+// $('#submit').click('')
+//   let $submit = $('#submit');
+//   $submit.click('legend').text('clicked')
+//   let $name = $('#name-input')
+//   $form.submit(name);
+// console.log('working')
+//   function name(event) {
+//     console.log(event);
+//     event.preventDefault();
+//     event.stopImmediatePropagation();
+//
+//   }
