bcdcfb245f6dcfd4e0bc5efa653e5f0c16604d42
TJ Stubbs <stubbsrule@gmail.com>
2017-12-10 10:22:47 -0500

    created all questions in html,made life easier, added audio files

--- garbagecode.js
+++ garbagecode.js
@@ -51,3 +51,175 @@
 //     event.stopImmediatePropagation();
 //
 //   }
+// let questionArr = {
+//   question:'What is homer\'s catchphrase?',
+//   choices : ['Doh', 'yes', 'maybe', 'i donno'],
+//   rightAnswer: 'Doh'
+// };
+
+//   let idArr = {
+//     answer1:'answer-1',
+//     answer2:'answer-2',
+//     answer3:'answer-3',
+//     answer4:'answer-4',
+//   }
+// function createQuestion(){
+//   let changeQ = questionArr.question;
+//   $("#question").html(changeQ);
+//   let changeAnswer = (questionArr.choices);
+//   let answerOne = $('#answer-1').text(changeAnswer);
+//   let answerTwo = $('#answer-2');
+//   let answerThree = $('#answer-3');
+//   let answerFour = $('#answer-4');
+//   let $choice = $(".answer");
+//
+//
+// // for (var i = 0; i < idArr.length; i++) {
+// // let
+// // }
+// }
+// createQuestion();
+  // let $choose = questionArr.choices;
+  // $grabDiv.each(function(i){
+  //
+  //
+  //
+  // });
+  //   function createButtons(index) {
+  //       let butList = $('<button>');
+  //       let item;
+  //       let input = '';
+  //       for (let i = 0; i < questionArr[index].choices.length; i++) {
+  //         item = $('<li>');
+  //         input = '<i class="button answer-button" value=' + i + ' />';
+  //         input += questions[index].choices[i];
+  //         item.append(input);
+  //         butList.append(item);
+  //       }
+  //       return butList;
+  //     }
+  //
+  //
+  //
+  //   //   function createQuestion(){
+  //
+  //   //     let choices = questionArr.choices;
+  //   //       // console.log(choices);
+  //   //
+  //   // for (var i = 0; i < choices.length; i++) {
+  //   // // console.log(choices[i])
+  //   // let btn = $("<button/>");
+  //   // let $grabDiv = $(".answer");
+  //   //  let newButton = $($grabDiv).append(btn)  //.html(choices);
+  //   //  btn.attr({
+  //   //     "id": i,
+  //   //     "class": "button answer-button"});
+  //   //
+  //   //     $(btn[i]).text(choices[i]);
+  // //   for (var i = 0; i < choices.length; i++) {
+  // // console.log(choices[i])
+  // //   btn.html(choices[i])
+  // //   }
+  // //newButton.text(choices[i])
+  //
+  // // }
+  // //       }
+  // //   createQuestion();
+  // class Questions {
+  // constructor(question, rightAnswer, choices, correct ){
+  //   this.question = question,
+  //   this.rightAnswer = rightAnswer,
+  //   this.choices = [],
+  //   // this.difficulty = 'high',
+  //   this.correct = false;
+  //   }
+  // isCorrect() {
+  //   this.correct = true;
+  // }
+    // createQuestion(){
+    // let q = $('#question');
+    // let a = $('#answer-button');
+    // console.log('create Question Working')
+    // }
+
+  //  }
+  // end of question class
+  // Shows question and answers
+  // let q1 = new Questions("What is Homer's catchphrase?", "D'oh",["Eat My Shorts","Aye Caramba", "That's Illogical","D'oh"]),
+  // const quest = new Questions()
+
+
+//creates new choice button
+//let $createQuestion = $('div').addClass('answer').append(`<button class='button'>Say what!? </button>`);
+//let $newQuestion = $('question-wrapper');
+
+// qqqq.addClass('question') = $('div').addClass('answer').append(`<button class='button'>Say what!? </button>`);
+
+
+    //   for (var i = 0; i < choices.length; i++) {
+    //     let newChoice = $('#ans')
+    //     let $choice = $(this[i]);
+    //     $choice[i].appendTo('')
+    //
+    // }
+
+
+  //quest.createQuestion();
+  // let question = [
+  //     new Questions("What is Homer's catchphrase?", "D'oh",["Eat My Shorts","Aye Caramba", "That's Illogical"]),
+  //     new Questions("Who is Bart's nemesis?", "Sideshow Bob",["Sideshow Mel","Bumblebee Man", "Nelson Muntz"]),
+  //     new Questions("What is the cause of and solution to all of life's problems according to Homer?", "Beer",["Pretzels","Doughnuts", "Marge"])
+  // ];
+
+
+  //changes text of #question
+   // let $changeQuestion = $('#question');
+   //  $changeQuestion.text('new text');
+
+  // for(i=0;i<answer.length;i++){
+  //    let random = Math.floor(Math.random() * 4)
+  // }
+//   $("#question").click(function(){
+//
+//   }
+ // $changeQuestion.innerHTML('new question');
+
+
+
+
+ // $('#question').show();
+  //css('display','block');
+  // classes
+
+ //  class Game {
+ //   constructor(startGame, questionPrompt, finishGame, restart){
+ //     this.startGame = startGame;
+ //     this.questionPrompt = questionPrompt;
+ //     this.finishGame = finishGame;
+ //     this.restart = restart;
+ //   }
+ //    startGame() {
+ //     body.onload()
+ //     }
+ //   }
+
+
+ //    class Questions {
+ //   constructor(question, rightAnswer, wrongAnswer, correct ){
+ //     this.question = question,
+ //     this.rightAnswer = rightAnswer,
+ //     this.wrongAnswer = wrongAnswer,
+ //     // this.difficulty = 'high',
+ //     this.correct = false;
+ //     }
+
+ //   isCorrect() {
+ //     this.correct = true;
+
+ //   }
+ //     }
+ //   let question = [
+ //       new Questions("What is Homer's catchphrase?", "D'oh",["Eat My Shorts","Aye Caramba", "That's Illogical"]),
+ //       new Questions("Who is Bart's nemesis?", "Sideshow Bob",["Sideshow Mel","Bumblebee Man", "Nelson Muntz"]),
+ //       new Questions("What is the cause of and solution to all of life's problems according to Homer?", "Beer",["Pretzels","Doughnuts", "Marge"])
+ //   ];
--- main.js
+++ main.js
@@ -1,170 +1,72 @@
 $(document).ready(()=> {
-  let questionArr = {
-    question:'What is homer\'s catchphrase?',
-    choices : ['Doh', 'yes', 'maybe', 'i donno'],
-    rightAnswer: 'Doh'
-  };
-  let $grabDiv = $(".answer");
-  let $choose = questionArr.choices;
-  $grabDiv.each(function(i){
-   let btn = $("<button/>");
-   console.log('working1');
-   btn.attr({
-     "id": i,
-     "class": "button answer-button"});
-     console.log('working2');
-   $($grabDiv).append(btn);
-   console.log('working3');
-  });
-console.log(questionArr[1].length)
-// for (var i = 0; i < array.length; i++) {
-//   array[i]
-// }
 
-    function createQuestion(){
-      let changeQ = questionArr.question;
-      $("#question").html(changeQ);
-      let choices = questionArr.choices;
-      }
-  createQuestion();
-  // class Game {
-  //   constructor() {
-  //    this.players = [];
-  //    // this method needs to be bound as it's called as a callback on .click()
-  //    this.createPlayers = this.createPlayers.bind(this)
-  //   }
-  //   init(){
-  //     $('#submit').click(this.createPlayers)
-  //   }
-  //   createPlayers(){
-  //     let name = $('#name-input').val()
-  //     this.players.push(new Player(name))
-  //     $('.players').append('<div>' + this.players[this.players.length-1].name + '</div>')
-  //     $('.players').show();
-  //     $('.hide-button').show();
-  //     $('legend').hide();
-  //     $('#submit').hide();
-  //     $('#name-input').hide();
-  //
-  //   }
-  // }
-  // class Player {
-  //   constructor(name) {
-  //     this.name = name;
-  //     this.position = 0;
-  //     this.won = false;
-  //   }
-  //   move() {
-  //   }
-  // }
-  // //end of game Class
-  // //calling game class
-  // let game = new Game()
-  // //initializing Game class to activate event listener to create player
-  // game.init()
+  class Game {
+    constructor() {
+     this.players = [];
+     // this method needs to be bound as it's called as a callback on .click()
+     this.createPlayers = this.createPlayers.bind(this)
+      this.startGame = this.startGame.bind(this)
+    }
+    init(){
+      $('#submit').click(this.createPlayers)
+      $('#start-game').click(this.startGame)
+      $('.correct-answer').click(this.rightAnswer)
+      $('.wrong-answer').click(this.wrongAnswer)
+      $('#next').click(this.nextQuestion)
+    }
+    createPlayers(){
+      let name = $('#name-input').val()
+      this.players.push(new Player(name))
+      $('.players').append('<div>' + this.players[this.players.length-1].name + '</div>')
+      $('.players').show();
+      $('.hide-button').show();
+      $('legend').hide();
+      $('#submit').hide();
+      $('#name-input').hide();
 
-  //$('#start-game').click(createQuestion);
+    }
+    startGame(){
+$('#start').hide();
+$('#question-1').show();
+      console.log('click');
+    }
+    rightAnswer(){
+$('.correct-answer').css('background-color','green');
+$('#next').show();
+return player.value ++;
+    }
+    wrongAnswer(){
+      $('.wrong-answer').css('background-color','red');
+      $('#next').show();
+    }
+    nextQuestion(){
+      $('#question-2').show();
+      $('#question-1').hide();
+      $('#next').hide();
+    }
+  }
+  class Player {
+    constructor(name) {
+      this.name = name;
+      this.value = 0;
+      this.won = false;
+    }
+    nextQuestion() {
+    }
+  }
+  //end of game Class
+  //calling game class
+  let game = new Game()
+  //initializing Game class to activate event listener to create player
+  game.init()
 
-  // class Questions {
-  // constructor(question, rightAnswer, choices, correct ){
-  //   this.question = question,
-  //   this.rightAnswer = rightAnswer,
-  //   this.choices = [],
-  //   // this.difficulty = 'high',
-  //   this.correct = false;
-  //   }
-  // isCorrect() {
-  //   this.correct = true;
-  // }
-    // createQuestion(){
-    // let q = $('#question');
-    // let a = $('#answer-button');
-    // console.log('create Question Working')
-    // }
-
-  //  }
-  // end of question class
-  // Shows question and answers
-  // let q1 = new Questions("What is Homer's catchphrase?", "D'oh",["Eat My Shorts","Aye Caramba", "That's Illogical","D'oh"]),
-  // const quest = new Questions()
-
-
-//creates new choice button
-//let $createQuestion = $('div').addClass('answer').append(`<button class='button'>Say what!? </button>`);
-//let $newQuestion = $('question-wrapper');
-
-// qqqq.addClass('question') = $('div').addClass('answer').append(`<button class='button'>Say what!? </button>`);
-
-
-    //   for (var i = 0; i < choices.length; i++) {
-    //     let newChoice = $('#ans')
-    //     let $choice = $(this[i]);
-    //     $choice[i].appendTo('')
-    //
-    // }
-
-
-  //quest.createQuestion();
-  // let question = [
-  //     new Questions("What is Homer's catchphrase?", "D'oh",["Eat My Shorts","Aye Caramba", "That's Illogical"]),
-  //     new Questions("Who is Bart's nemesis?", "Sideshow Bob",["Sideshow Mel","Bumblebee Man", "Nelson Muntz"]),
-  //     new Questions("What is the cause of and solution to all of life's problems according to Homer?", "Beer",["Pretzels","Doughnuts", "Marge"])
-  // ];
-
-
-  //changes text of #question
-   // let $changeQuestion = $('#question');
-   //  $changeQuestion.text('new text');
-
-  // for(i=0;i<answer.length;i++){
-  //    let random = Math.floor(Math.random() * 4)
-  // }
-//   $("#question").click(function(){
-//
-//   }
- // $changeQuestion.innerHTML('new question');
+  // $('start-game').on( 'click', function() {
+  // console.log( "click" );
+  // });
 
 
 
-
- // $('#question').show();
-  //css('display','block');
 
 
 
 });
-
- // classes
-
-//  class Game {
-//   constructor(startGame, questionPrompt, finishGame, restart){
-//     this.startGame = startGame;
-//     this.questionPrompt = questionPrompt;
-//     this.finishGame = finishGame;
-//     this.restart = restart;
-//   }
-//    startGame() {
-//     body.onload()
-//     }
-//   }
-
-
-//    class Questions {
-//   constructor(question, rightAnswer, wrongAnswer, correct ){
-//     this.question = question,
-//     this.rightAnswer = rightAnswer,
-//     this.wrongAnswer = wrongAnswer,
-//     // this.difficulty = 'high',
-//     this.correct = false;
-//     }
-
-//   isCorrect() {
-//     this.correct = true;
-
-//   }
-//     }
-//   let question = [
-//       new Questions("What is Homer's catchphrase?", "D'oh",["Eat My Shorts","Aye Caramba", "That's Illogical"]),
-//       new Questions("Who is Bart's nemesis?", "Sideshow Bob",["Sideshow Mel","Bumblebee Man", "Nelson Muntz"]),
-//       new Questions("What is the cause of and solution to all of life's problems according to Homer?", "Beer",["Pretzels","Doughnuts", "Marge"])
-//   ];
